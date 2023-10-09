10527be76d612f36443a9a2502f80a5b47f30867
hongdagen <hongyixyz@163.com>
2023-10-09 20:54:33 +0800

    feat(rustlings): primitive

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(n:u32) -> u32{
+    if n > 40{
+        n
+    }else{
+        2*n
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
@@ -31,3 +35,4 @@ fn verify_test() {
     assert_eq!(41, price3);
     assert_eq!(65, price4);
 }
+
